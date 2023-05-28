631be2ba5ddb51c7c098f7c097e40854434e0ca8
Firstname Lastname <enzoesper7@gmail.com>
2023-05-28 18:38:07 -0300

    extracting some logic in a custom hook to fetch restaurants (results) from the yelp api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,15 @@
 import React from "react"
 import { Text, View, StyleSheet } from "react-native"
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp"
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = React.useState("");
-  const [results, setResults] = React.useState([]);
-  const [errorMessage, setErrorMessage] = React.useState("");
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose"
-        }
-      });
-      setResults(response.data.businesses);
-      setErrorMessage("");
-    } catch (error) {
-      console.log("searchApi error", error);
-      setErrorMessage("Something went wrong :(");
-    }
-  }
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
-      <SearchBar searchTerm={searchTerm} onChangeSearchTerm={setSearchTerm} onTermSubmit={searchApi}/>
+      <SearchBar searchTerm={searchTerm} onChangeSearchTerm={setSearchTerm} onTermSubmit={() => searchApi(searchTerm)}/>
       {errorMessage && <Text>{errorMessage}</Text>}
       <Text>We have found {results.length} results</Text>
     </View>
