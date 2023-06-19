e905be8a6a80eae0322c0fde2199e4aa904a0325
Firstname Lastname <enzoesper7@gmail.com>
2023-06-19 20:55:49 -0300

    adding styles to show the lists

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 function ResultsDetail({ result }) {
   return (
-    <View>
-      <Text>{result.name}</Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15,
+  },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+    marginBottom: 5,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+});
 
 export default ResultsDetail;
