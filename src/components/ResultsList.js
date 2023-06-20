957b1d053b1332a312d63a7578dceb0693ad3c89
Firstname Lastname <enzoesper7@gmail.com>
2023-06-19 23:23:40 -0300

    fix: validating results before render

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -6,6 +6,10 @@ import { useNavigation } from '@react-navigation/native';
 const ResultsList = ({ title, results }) => {
   const navigation = useNavigation();
 
+  if (!results.length) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
