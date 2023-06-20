490e55b8ef3f882c56f0c2f327226294958649f6
Firstname Lastname <enzoesper7@gmail.com>
2023-06-19 21:06:22 -0300

    constraining the view to be able to scroll until the last list and view properly its content. Without this propr, the scroll works but it stops at certain point avoiding to see all the lists properly

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -16,7 +16,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <SearchBar
         searchTerm={searchTerm}
         onChangeSearchTerm={setSearchTerm}
@@ -27,6 +27,7 @@ const SearchScreen = () => {
       <ScrollView>
         <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
         <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
         <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
       </ScrollView>
     </View>
