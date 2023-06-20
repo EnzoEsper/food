5aae09c8ca1ddd4d40e9aa035a90a92b154fd991
Firstname Lastname <enzoesper7@gmail.com>
2023-06-19 23:20:29 -0300

    communicating the id parameter. fetching a single restaurant and displaying the images of it

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,13 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import yelp from '../api/yelp';
 
 function ResultsShowScreen() {
+  const route = useRoute();
+  const id = route.params.id;
+
+  const [restaurant, setRestaurant] = useState(null);
+
+  const getRestaurant = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setRestaurant(response.data);
+  };
+
+  useEffect(() => {
+    getRestaurant(id);
+  }, []);
+
+  if (!restaurant) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>Results Show</Text>
+      <Text>{restaurant.name}</Text>
+      <FlatList
+        data={restaurant.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 300,
+    height: 200,
+  },
+});
 
 export default ResultsShowScreen;
